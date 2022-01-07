43b1c2ed37957390742eb52da56378a214178595
Seoho Jung <seoho91@gmail.com>
2022-01-07 16:15:16 +0100

    [seoho] refactor createProgram.js

--- components/ProgramHeader.js
+++ components/ProgramHeader.js
@@ -15,63 +15,39 @@ const TitleContainer = styled.View`
   align-items: center;
 `;
 
-const TitleInput = styled.TextInput`
-  color: ${(props) => props.theme.fontColor};
-  font-size: 16px
-  border-radius: 5px;
-  margin-bottom: 5px;
-`;
+// const TitleInput = styled.TextInput`
+//   color: ${(props) => props.theme.fontColor};
+//   font-size: 16px;
+//   border-radius: 5px;
+//   margin-bottom: 5px;
+// `;
 
-const DescriptionInput = styled.TextInput`
-  color: ${(props) => props.theme.fontColor};
-  background-color: ${(props) => props.theme.lightgray};
-  padding: 10px 15px;
-  font-size: 14px
-  border-radius: 5px;
-  height: 40px;
-`;
+// const DescriptionInput = styled.TextInput`
+//   color: ${(props) => props.theme.fontColor};
+//   background-color: ${(props) => props.theme.lightgray};
+//   padding: 10px 15px;
+//   font-size: 14px;
+//   border-radius: 5px;
+//   height: 40px;
+// `;
 
 const ToggleContainer = styled.View`
-margin-left: 35px
-align-items: center;
+  margin-left: 35px;
+  align-items: center;
 `;
+
 const ToggleSwitch = styled.View`
   margin-top: 7px;
 `;
 
-export default function ProgramHeader() {
-  const { register, setValue, getValues, control } = useForm();
+function ProgramHeader({ children }) {
+  // const { register, setValue, getValues, control, watch } = useForm();
   const [isPrivate, setIsPrivate] = useState(false);
   const toggleSwitch = () => setIsPrivate((previousState) => !previousState);
 
   return (
     <HeaderContainer>
-      <TitleContainer>
-        <Controller
-          name="programTitle"
-          control={control}
-          rules={{ required: true }}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <TitleInput
-              placeholder="프로그램 이름"
-              placeholderTextColor="#797d7f"
-              onChangeText={(text) => setValue("programTitle", text)}
-            />
-          )}
-        />
-        <Controller
-          name="description"
-          control={control}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <DescriptionInput
-              placeholder="프로그램 설명"
-              placeholderTextColor="#797d7f"
-              multiline={true}
-              onChangeText={(text) => setValue("description", text)}
-            />
-          )}
-        />
-      </TitleContainer>
+      <TitleContainer>{children}</TitleContainer>
 
       <ToggleContainer>
         <ColorText>프로그램 공개</ColorText>
@@ -89,3 +65,5 @@ export default function ProgramHeader() {
     </HeaderContainer>
   );
 }
+
+export default ProgramHeader;
--- screens/CreateProgram.js
+++ screens/CreateProgram.js
@@ -10,10 +10,47 @@ import ProgramHeader from "../components/ProgramHeader";
 import ProgramTemplate from "../components/ProgramTemplate";
 import DismissKeyboard from "../components/DismissKeyboard";
 import AddWorkoutButton from "../components/AddWorkoutButton";
+import { Switch } from "react-native";
 
 const Container = styled.View`
   padding: 0 15px;
   margin-top: 15px;
+`;
+
+const HeaderContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding: 0 35px;
+  margin-bottom: 15px;
+`;
+
+const TitleContainer = styled.View`
+  align-items: center;
+`;
+
+const ToggleContainer = styled.View`
+  margin-left: 35px;
+  align-items: center;
+`;
+
+const ToggleSwitch = styled.View`
+  margin-top: 7px;
+`;
+
+const TitleInput = styled.TextInput`
+  color: ${(props) => props.theme.fontColor};
+  font-size: 16px;
+  border-radius: 5px;
+  margin-bottom: 5px;
+`;
+
+const DescriptionInput = styled.TextInput`
+  color: ${(props) => props.theme.fontColor};
+  background-color: ${(props) => props.theme.lightgray};
+  padding: 10px 15px;
+  font-size: 14px;
+  border-radius: 5px;
+  height: 40px;
 `;
 
 const CREATE_PROGRAM_MUTATION = gql`
@@ -65,34 +102,44 @@ const CREATE_TEMPLATE_SET_MUTATION = gql`
 `;
 
 export default function CreateProgram() {
-  const { register, handleSubmit, setValue, getValues, watch, setError } =
-    useForm();
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    getValues,
+    control,
+    watch,
+    setError,
+  } = useForm();
 
-  const onCreateTemplateSetCompleted = (data) => {
-    const {
-      createTemplateSet: { ok, id, error },
-    } = data;
-    if (!ok) {
-      setError("result", {
-        message: error,
-      });
-    }
-  };
+  const [isPrivate, setIsPrivate] = useState(false);
 
-  const onCreateTemplateCompleted = (data) => {
-    const {
-      createTemplate: { ok, id: templateId, error },
-    } = data;
-    if (!ok) {
-      setError("result", {
-        message: error,
-      });
-    }
-    const { setCount } = getValues();
-    createTemplateSetFunction({
-      variables: { templateId, setCount },
-    });
-  };
+  const toggleSwitch = () => setIsPrivate((previousState) => !previousState);
+  // const onCreateTemplateSetCompleted = (data) => {
+  //   const {
+  //     createTemplateSet: { ok, id, error },
+  //   } = data;
+  //   if (!ok) {
+  //     setError("result", {
+  //       message: error,
+  //     });
+  //   }
+  // };
+
+  // const onCreateTemplateCompleted = (data) => {
+  //   const {
+  //     createTemplate: { ok, id: templateId, error },
+  //   } = data;
+  //   if (!ok) {
+  //     setError("result", {
+  //       message: error,
+  //     });
+  //   }
+  //   const { setCount } = getValues();
+  //   createTemplateSetFunction({
+  //     variables: { templateId, setCount },
+  //   });
+  // };
 
   const onCreateProgramCompleted = (data) => {
     const {
@@ -103,10 +150,10 @@ export default function CreateProgram() {
         message: error,
       });
     }
-    const { workoutTitle } = getValues();
-    createTemplateFunction({
-      variables: { programId, title: workoutTitle },
-    });
+    // const { workoutTitle } = getValues();
+    // createTemplateFunction({
+    //   variables: { programId, title: workoutTitle },
+    // });
   };
 
   const [createProgramFunction, { loading, error }] = useMutation(
@@ -116,16 +163,16 @@ export default function CreateProgram() {
     }
   );
 
-  const [createTemplateFunction] = useMutation(CREATE_TEMPLATE_MUTATION, {
-    onCompleted: onCreateTemplateCompleted,
-  });
+  // const [createTemplateFunction] = useMutation(CREATE_TEMPLATE_MUTATION, {
+  //   onCompleted: onCreateTemplateCompleted,
+  // });
 
-  const [createTemplateSetFunction] = useMutation(
-    CREATE_TEMPLATE_SET_MUTATION,
-    {
-      onCreateTemplateSetCompleted,
-    }
-  );
+  // const [createTemplateSetFunction] = useMutation(
+  //   CREATE_TEMPLATE_SET_MUTATION,
+  //   {
+  //     onCreateTemplateSetCompleted,
+  //   }
+  // );
 
   // onSubmitValid
   const onSubmitValid = (submissionData) => {
@@ -133,6 +180,7 @@ export default function CreateProgram() {
       return;
     }
     const { programTitle, description } = getValues();
+    console.log(programTitle, description);
     createProgramFunction({
       variables: { title: programTitle, description, isPrivate },
     });
@@ -141,9 +189,51 @@ export default function CreateProgram() {
   return (
     <DismissKeyboard>
       <Container>
-        <ProgramHeader />
-        <ProgramTemplate />
-        <AddWorkoutButton />
+        <HeaderContainer>
+          <TitleContainer>
+            <Controller
+              name="programTitle"
+              control={control}
+              rules={{ required: true }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <TitleInput
+                  placeholder="프로그램 이름"
+                  placeholderTextColor="#797d7f"
+                  onChangeText={(text) => setValue("programTitle", text)}
+                />
+              )}
+            />
+            <Controller
+              name="description"
+              control={control}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <DescriptionInput
+                  placeholder="프로그램 설명"
+                  placeholderTextColor="#797d7f"
+                  multiline={true}
+                  onChangeText={(text) => setValue("description", text)}
+                />
+              )}
+            />
+          </TitleContainer>
+
+          <ToggleContainer>
+            <ColorText>프로그램 공개</ColorText>
+            <ToggleSwitch>
+              <Switch
+                trackColor={{ true: "#42a5f5" }}
+                // thumbColor="#42a5f5"
+                style={{ transform: [{ scaleX: 0.9 }, { scaleY: 0.9 }] }}
+                ios_backgroundColor="#cacfd2"
+                onValueChange={toggleSwitch}
+                value={isPrivate}
+              />
+            </ToggleSwitch>
+          </ToggleContainer>
+        </HeaderContainer>
+
+        {/* <ProgramTemplate />
+        <AddWorkoutButton /> */}
         <MainButton
           text="새 프로그램 저장"
           loading={loading}
