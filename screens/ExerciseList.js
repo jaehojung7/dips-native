5b97fc3ca5b2f46ba39bffafc366925b09e445aa
Seoho Jung <seoho91@gmail.com>
2022-05-23 16:36:01 +0200

    [seoho] Settings tab basics

--- screens/ExerciseList.js
+++ screens/ExerciseList.js
@@ -2,58 +2,29 @@ import React, { useState } from "react";
 import styled from "styled-components/native";
 import DeleteExercise from "../components/DeleteExercise";
 import { FontAwesome5 } from "@expo/vector-icons";
-import { gql, useQuery } from "@apollo/client";
-import { FlatList, ActivityIndicator } from "react-native";
-
-const ME_QUERY = gql`
-  query me {
-    me {
-      id
-      exercises {
-        id
-        exercise
-        bodyPart
-      }
-    }
-  }
-`;
-
-const IndicatorContainer = styled.View`
-  flex: 1;
-  justify-content: center;
-`;
+import { FlatList } from "react-native";
 
 const HeaderContainer = styled.View`
   margin: 40px 25px 15px 15px;
+  align-items: center;
+`;
+
+const ButtonContainer = styled.TouchableOpacity`
   flex-direction: row;
   align-items: center;
-  justify-content: space-between;
+  margin-top: 50px;
 `;
-
-const Header = styled.Text`
-  color: ${(props) => props.theme.mainColor};
-  font-size: 25px;
-  font-weight: 700;
-`;
-
-const ButtonContainer = styled.TouchableOpacity``;
 
 const ButtonText = styled.Text`
   color: ${(props) => props.theme.mainColor};
+  font-size: 18px;
   font-weight: 600;
-  text-align: right;
 `;
 
-export default function Exercise({ navigation }) {
-  const { data, loading, refetch } = useQuery(ME_QUERY);
-  if (loading)
-    return (
-      <IndicatorContainer>
-        <ActivityIndicator />
-      </IndicatorContainer>
-    );
-  const user = data?.me;
-  const exercises = data?.me?.exercises;
+export default function ExerciseList({ navigation, route }) {
+  const { userId } = route.params;
+  const { exercises } = route.params;
+
   const [refreshing, setRefreshing] = useState(false);
   const refresh = async () => {
     setRefreshing(true);
@@ -68,13 +39,14 @@ export default function Exercise({ navigation }) {
   return (
     <>
       <HeaderContainer>
-        <Header>Exercise</Header>
+        {/* <Header>Exercise</Header> */}
         <ButtonContainer
-          onPress={() => navigation.navigate("CreateExercise", { user })}
+          onPress={() => navigation.navigate("CreateExercise", { userId })}
         >
           <ButtonText>
             <FontAwesome5 name="plus" size={16} />
           </ButtonText>
+          <ButtonText style={{ marginLeft: 10 }}>Create exercise</ButtonText>
         </ButtonContainer>
       </HeaderContainer>
 
