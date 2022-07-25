22c5fe813fb0604f31b89322229a46c63337f41e
Jaeho <jthyxz4@gmail.com>
2022-07-26 00:42:12 +0900

    [hoje] expo cli for deployment

--- apollo.js
+++ apollo.js
@@ -25,7 +25,7 @@ export const logUserOut = async () => {
 };
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000/graphql",
+  uri: "https://dips-backend.herokuapp.com/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
